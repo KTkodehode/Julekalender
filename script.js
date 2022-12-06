982bf8e4215aa3523cbc2a0d9447f2503fe8c9a3
Kenneth <kenneth1.kodehode@gmail.com>
2022-12-06 22:45:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// ** Snowing ** //
+for(let i = 0; i < 50; i++) {
+    let snow = document.createElement('div');
+    let po = Math.floor(Math.random()*100);
+    let d = Math.floor(Math.random()*10000);
+    let snowBlur = Math.floor(Math.random()* 15);
+  
+    snow.id = 'snow';
+    snow.style.left = po + '%';
+    snow.style.animationDuration = d + 'ms';
+    snow.style.filter = `blur(${snowBlur +2}px)`;
+  
+    document.body.appendChild(snow);
+  }
+
+
+  
+
+// Get the modal
+let modal = document.getElementById("myModal");
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+  modal.style.display = "none";
+}
+}
+
+document.getElementById("notToday").src="./img/grinch-yes-no.gif";
+
+
+
+
+
+  
+  // // ** Lager og Appender til div
+  // const imgCont = document.querySelector(".imageContainer");
+  
+  // for (let i = 1; i < 25; i++){
+  //   const img = document.createElement("img");
+  //   img.src = `/baubels/b${i}.png`;
+    
+  //   imgCont.append(img);
+  
+  // }
+  
+  
+
+    // const imgCont = document.querySelector(".imageContainer");
+
+    // for (let i = 1; i < 25; i++){
+    //     const img = document.createElement("img");
+    //     img.src = `/baubels/b${i}.png`;
+        
+    //     imgCont.append(img);
+
+    // }
+
+// var d = new Date();
+// var todaysDate = d.getDate();
